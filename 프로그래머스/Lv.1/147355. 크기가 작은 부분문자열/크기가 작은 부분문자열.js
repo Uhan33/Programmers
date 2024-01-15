bad9594d2da8cf3ae8592df768aa0811645208a9
Uhan33 <116422670+Uhan33@users.noreply.github.com>
2024-01-15 09:49:05 +0900

    [level Lv.1] Title: 크기가 작은 부분문자열, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,9 @@
+function solution(t, p) {
+    let answer = 0;
+    
+    for(let i = 0; i <= t.length-p.length; i++) {
+        Number(t.substr(i, p.length)) <= Number(p) ? answer++ : 0;
+    }
+    
+    return answer;
+}
