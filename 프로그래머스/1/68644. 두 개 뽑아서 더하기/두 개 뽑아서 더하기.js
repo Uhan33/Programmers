5b7be132344efdce6c74debe7b394b2fadc6daa0
Uhan33 <116422670+Uhan33@users.noreply.github.com>
2024-01-22 09:15:47 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.51 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -0,0 +1,28 @@
+function solution(numbers) {
+    let answer = [];
+    
+    if(numbers.length === 2)
+        answer.push(numbers[0] + numbers[1]);
+    else {
+        for(let i = 0; i < numbers.length-1; i++) {
+            for(let j = i+1; j < numbers.length; j++) {
+                // if(!answer.find(e => e === numbers[i] + numbers[j]))
+                    answer.push(numbers[i] + numbers[j]);
+            }   
+        }
+        
+        answer = [...new Set(answer)];
+    
+        for(let i = 0; i < answer.length-1; i++) {
+            for(let j = i+1; j < answer.length; j++) {
+                if(answer[i] > answer[j]) {
+                    let temp = answer[j];
+                    answer[j] = answer[i];
+                    answer[i] = temp;
+                }
+            }
+        }
+    }
+    
+    return answer;
+}
